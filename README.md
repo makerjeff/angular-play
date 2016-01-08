652f4249082ac2824e330bf4552612a2e01dc264
Jefferson Wu <makerjeffwu@gmail.com>
2016-01-08 13:36:04 -0800

    completed basic ui-router demo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 #Angular Play
-Angular Experiments
+Angular Experiments, UI-Router, custom Directives, etc.
 
 ###Tutorials Base:
 [Angular UI-Router](https://scotch.io/tutorials/angular-routing-using-ui-router)
+
+###Start-up Instructions:
+1. Installed nodemon with ```npm install -g nodemon``` (-g means 'globally').
+2. Run ```server.js``` in Nodemon with ```nodemon server.js <port>```. Without the port argument, it'll default to 8000.
--- public/js/core.js
+++ public/js/core.js
@@ -1,8 +1,116 @@
-//ANGULAR-PLAY CORE JS file
+/**
+ * ANGULAR PLAY - CORE.JS
+ */
 
-app.directive('ngSparkline', function(){
-    return {
-        restrict: 'E',
-        template: '<div class="sparkline"></div>'
-    }
+// TODO Separate out for production
+
+// == APP ==
+var app = angular.module('cstApp', ['ui.router']);
+
+// == configure routes ==
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+    //default to this location
+    $urlRouterProvider.otherwise('/home');
+
+    //home state
+    $stateProvider.state('home', {
+        url:'/home',
+        templateUrl:'partials/partial-home.html'
+    });
+
+    //nested state with custom controller
+    $stateProvider.state('home.list', {
+        url:'/list',
+        templateUrl: 'partials/partial-home-list.html',
+        controller: function($scope) {
+            $scope.dogs = ['Bernese', 'Husky', 'GoldenDoodle'];
+        }
+    });
+
+    //nested state with random string data
+    $stateProvider.state('home.paragraph', {
+        url: '/paragraph',
+        template: 'I could sure use a nice drink right now.'
+
+    });
+
+    //about
+    $stateProvider.state('about', {
+        url:'/about',
+        views: {
+            //main template place here (relatively named)
+            '': {templateUrl: 'partials/partial-about.html'},
+
+            // child views defined here, absolutely named
+            //'columnOne@about': {
+            //    template: 'Column One Text!'
+            //},
+            'columnOne@about': {
+                templateUrl: 'templates/left-table-data.html',
+                controller: 'carController'
+            },
+            'columnTwo@about': {
+                templateUrl: 'templates/right-table-data.html',
+                controller: 'mainController'
+            }
+
+        }
+    });
+
+    //uncomment this for no-hash routes (not 100% working)
+    //$locationProvider.html5Mode(true);
+
+});// == configure routes - END ==
+
+
+// ===== CONTROLLERS =====
+
+// == Main Controller ==
+app.controller('mainController', function($scope, $timeout){
+    $scope.message = 'testingulars';
+    $scope.scotches = [
+        {
+            name:'Macallan 12',
+            price: 50
+        },
+        {
+            name:'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name:'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+
+    // TODO TWITTER FIX
+    //$timeout = twttr.widgets.load();
 });
+
+// == Car Specific Controller ==
+app.controller('carController', ['$scope','$timeout', function($scope, $timeout){
+    //data for dummy scope
+    $scope.car = [
+        {
+            model:'Toyota Corolla',
+            price: 12000
+        },
+        {
+            model:'Nissan 240SX',
+            price: 2900
+        },
+        {
+            model:'Toyota Matrix',
+            price: 15800
+        },
+        {
+            model: 'Toyota Corolla',
+            price: 16000
+        },
+        {
+            model: 'Toyota Pickup',
+            price: 12000
+        }
+    ];
+}]);
+
